34c6847ee51cf98a06129d061dfdac3ed9485c3b
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-05 23:36:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,18 +6,48 @@ class Student
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
-    new_student
+    new_students
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM students 
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+      
   end
 
   def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+  end
+
+  def save
     sql = <<-SQL
-      SELECT * FROM students
-      WHERE name = ?
-      LIMIT 1
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql, name).map { |row|
-      self.new_from_db(row)
-    }.first
-    end
+    DB[:conn].execute(sql, self.name, self.grade)
   end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+end
