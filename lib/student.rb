6805602ceaf4c0cfb204d8137710308711c47cdc
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-06 00:12:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,17 @@ class Student
       end
    end
 
-   def self.first_X_students_in_grade_10
+   def self.first_X_students_in_grade_10(name)
+     sql = <<-SQL
+      SELECT * FROM students 
+      WHERE grade = 10
+    SQL
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+   end 
+    
+   end 
 
   def save
     sql = <<-SQL
