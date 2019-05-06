24385fb20c406450aa1a447fb9c03a422b8df85b
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-06 00:15:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ class Student
       end
    end
 
-   def self.first_student_in_grade_10(name)
+   def self.first_student_in_grade_10
      sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
