57ebe4b537be1110f54b79505d16e32710f7e2e7
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-06 00:12:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,8 @@ class Student
       SELECT * FROM students
       WHERE grade = 10
     SQL
-      DB[:conn].execute(sql).map do |row|
+
+      DB[:conn].execute(sql, name).map do |row|
         self.new_from_db(row)
       end
    end 
